b4bbe43c1d7f7e6511a8fd2affe06f39a6eff190
yipu0v0 <firstawhois@gmail.com>
2021-12-01 22:23:43 +0800

    DumplingPeer2PeerTradeV1 init

---
+++ contracts/DumplingPeer2PeerTradeV1.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract DumplingPeer2PeerTradeV1 is Ownable {
+    uint256 public _tradeCounter = 0;
+
+    function CreateTrade(
+        address tokenAddress,
+        uint256 tokenId,
+        address acceptor,
+        uint256 royalty,
+        uint256 donation,
+        uint256 durationInSec
+    ) public returns (uint256/* trade ID */) {
+        _tradeCounter++;
+        return _tradeCounter;
+    }
+
+    function CancelTrade(uint256 tradeId) public {}
+
+    function AcceptTrade(uint256 tradeId) public payable {}
+}
